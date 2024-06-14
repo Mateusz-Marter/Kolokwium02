a58647edce4a7fba6574f0a1d4e490e2021da9c7
s27660 <s27660@pjwstk.edu.pl>
2024-06-14 10:07:23 +0200

    Fina

--- Kolokwium02/Kolokwium02/Controllers/SuscriptionController.cs
+++ Kolokwium02/Kolokwium02/Controllers/SuscriptionController.cs
@@ -6,15 +6,39 @@ namespace Kolokwium02.Controllers;
 public class SubscriptionController : ControllerBase
 {
     private readonly ISubscriptionService _subscriptionService;
+    private readonly IClientService _clientService;
 
-    public SubscriptionController(ISubscriptionService subscriptionService)
+    public SubscriptionController(ISubscriptionService subscriptionService, IClientService clientService)
     {
         _subscriptionService = subscriptionService;
+        _clientService = clientService;
     }
 
     [HttpPut]
     public async Task<IActionResult> GetSubsciptionPayment(int IdClient, int IdSubcripton, decimal Payment)
     {
+        
+        if (!await _clientService.ClientExists(IdClient))
+        {
+            return NotFound("no found client");
+        }
+        =
+        if (!await _subscriptionService.SubscriptionExists(IdSubcripton))
+        {
+            return NotFound("no found subscription");
+        }
+        =
+        if (!await _subscriptionService.SubscriptionActives(IdSubcripton))
+        {
+            return BadRequest("Subscription is not active");
+        }
+        
+        if (!await _subscriptionService.IsSubscriptionPaid(IdSubcripton))
+        {
+            return BadRequest("subscription is already active");
+        }
+        
+        
         return Ok();
     }
 }
--- Kolokwium02/Kolokwium02/Service/SubscriptionService.cs
+++ Kolokwium02/Kolokwium02/Service/SubscriptionService.cs
@@ -3,7 +3,9 @@
 
 public interface ISubscriptionService
 {
-    
+    Task<bool> SubscriptionExists(int idSubcripton);
+    Task<bool> SubscriptionActives(int idSubcripton);
+    Task<bool> IsSubscriptionPaid(int idSubcripton);
 }
 
 public class SubscriptionService
